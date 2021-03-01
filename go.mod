bb498e1b5ee2e3a7ce6e1fa7a611e56d1fc3e52c
Ken Morel Santana <ken.morel@hotmail.com>
2021-03-01 15:48:54 -0400

    Basic cron scheduler setup
    Initialiaze the cron scheduler with a simple counter server (To be change later)

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/amanjaiswalofficial/ano
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-co-op/gocron v0.7.0 // indirect
+	github.com/sirupsen/logrus v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/amanjaiswalofficial/ano/src"
+	"github.com/gin-gonic/gin"
+	"github.com/go-co-op/gocron"
+)
 
 func main() {
 	r := gin.Default()
-
 	r.GET("/feed", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "success",
-			"feed":    gin.H{},
+			"N":       src.N,
 		})
 	})
+
+	// Cron Scheduler setup
+	sched := gocron.NewScheduler(time.UTC)
+	sched.Every(3).Seconds().Do(src.Count)
+
+	// Start the scheduler without blocking the current goroutine
+	sched.StartAsync()
 
 	r.Run(":8080")
 }
