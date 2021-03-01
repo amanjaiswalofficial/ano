bc3abcbce0af57e122ec6bbf3590dad85f1d8025
Aman Jaiswal <amanjai01@gmail.com>
2021-03-02 00:01:17 +0530

    Layout of feed for homepage

---
+++ frontend/src/components/feed/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Row } from 'react-grid-system';
+
+const Feed = ({classes}) => {
+    return (
+        <Row lg={12} justify="center" className={classes.contentRow}>
+                    Data goes here
+        </Row>
+    )
+}
+
+export default Feed
